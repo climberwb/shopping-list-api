51b1ec97b16b6879710e78d8ed30214ee12f592b
Warren Kushner <warren.kushner@gmail.com>
2015-07-20 22:53:03 +0000

    added test for edge case

--- test/test-server.js
+++ test/test-server.js
@@ -106,7 +106,33 @@ describe('Shopping List', function(){
             });
     });
     
-   // it('should add an item where edit item does not exist');
+    it('should add an item where edit item does not exist', function(done){
+        chai.request(app)
+            .put('/items/20')
+            .send({name:'tacos',id:20})
+            .end(function(err,res){
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.name.should.equal('tacos');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(4);
+                storage.items[3].should.be.a('object');
+                storage.items[3].should.have.property('id');
+                storage.items[3].should.have.property('name');
+                storage.items[3].id.should.be.a('number');
+                storage.items[3].name.should.be.a('string');
+                storage.items[3].name.should.equal('tacos');
+                storage.items[3].id.should.equal(20);
+                done();
+            });
+    });
+    
 });
 
 
